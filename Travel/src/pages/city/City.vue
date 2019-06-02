ef26619c7ad9085a540b542dfa694ec3da0c1a20
964269669 <964269669@qq.com>
2019-06-02 19:50:06 +0800

    优化字母表滑动

--- Travel/src/pages/city/City.vue
+++ Travel/src/pages/city/City.vue
@@ -2,8 +2,8 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list :cities = "cities" :hot="hotCities"></city-list>
-    <city-alphabet :cities="cities"></city-alphabet>
+    <city-list :cities = "cities" :letter = "letter" :hot="hotCities"></city-list>
+    <city-alphabet :cities="cities" @change="handleLetterChange"></city-alphabet>
   </div>
 </template>
 
@@ -24,7 +24,8 @@ export default {
   data() {
     return {
       cities: {},
-      hotCities: []
+      hotCities: [],
+      letter: ''
     }
   },
   mounted() {
@@ -43,6 +44,11 @@ export default {
         this.cities = data.cities
         this.hotCities = data.hotCities
       }
+    },
+    handleLetterChange(letter) {
+      console.log(49, letter)
+      this.letter = letter
+      // this.$emit('toAl', alphabet)
     }
   }
 }
--- Travel/src/pages/city/components/List.vue
+++ Travel/src/pages/city/components/List.vue
@@ -2,27 +2,31 @@
   <div class="list" ref="wrapper">
     <div>
       <div class="area">
-      <div class="title border-topbottom">当前城市</div>
-      <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">北京</div>
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">热门城市</div>
-      <div class="button-list">
-        <div class="button-wrapper" v-for="item of hot" :key="item.id">
-          <div class="button">{{item.name}}</div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper" v-for="item of hot" :key="item.id">
+            <div class="button">{{item.name}}</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="area" v-for="(item, key) in cities" :key="key">
-      <div class="title border-topbottom">{{key}}</div>
-      <div class="item-list">
-        <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">{{innerItem.name}}</div>
+      <div class="area" v-for="(item, key) in cities" :key="key" :ref="key">
+        <div class="title border-topbottom">{{key}}</div>
+        <div class="item-list">
+          <div
+            class="item border-bottom"
+            v-for="innerItem of item"
+            :key="innerItem.id"
+          >{{innerItem.name}}</div>
+        </div>
       </div>
-    </div>
     </div>
   </div>
 </template>
@@ -33,10 +37,20 @@ export default {
   name: 'CityList',
   props: {
     hot: Array,
-    cities: Object
+    cities: Object,
+    letter: String
   },
   mounted() {
     this.scroll = new BScroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter(newVal, oldVal) {
+      if (this.letter) {
+        const ele = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(ele)
+      }
+      console.log(44, this.letter)
+    }
   }
 }
 </script>
@@ -55,21 +69,21 @@ export default {
   &:after
     border-color: #777
 .list
-  overflow hidden
+  overflow: hidden
   position: absolute
   top: 1.58rem
   left: 0
   right: 0
   bottom: 0
   .title
-    line-height: .54rem
+    line-height: 0.54rem
     background: #eeeeee
-    padding-left: .2rem
+    padding-left: 0.2rem
     color: #666666
-    font-size: .26rem
+    font-size: 0.26rem
   .button-list
     overflow: hidden
-    padding: .1rem .6rem .1rem .1rem
+    padding: 0.1rem 0.6rem 0.1rem 0.1rem
     .button-wrapper
       float: left
       width: 33.33%
@@ -77,10 +91,10 @@ export default {
         margin: 0.1rem
         padding: 0.1rem 0
         text-align: center
-        border: .02rem solid #ccc
-        border-radius: .06rem
+        border: 0.02rem solid #ccc
+        border-radius: 0.06rem
   .item-list
     .item
-      line-height .76rem
-      padding-left 0.2rem
+      line-height: 0.76rem
+      padding-left: 0.2rem
 </style>
