8c50c1a2d3bc21c68f8b4a74242813dac33be987
964269669 <964269669@qq.com>
2019-06-02 11:55:28 +0800

    city-router

--- Travel/src/pages/home/components/Header.vue
+++ Travel/src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{city}}
-      <span class="iconfont arrow-icon">&#xe601;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{city}}
+        <span class="iconfont arrow-icon">&#xe601;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -29,42 +31,33 @@ export default {
 </script>
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl'
-.header {
-  display: flex;
-  line-height: 0.86rem;
-  background-color: $bgColor;
-  color: #fff;
 
-  .header-left {
-    width: 0.62rem;
-    float: left;
-
-    .back-icon {
-      text-align: center;
-      font-size: 0.4rem;
-    }
-  }
-
-  .header-input {
-    flex: 1;
-    height: 0.64rem;
-    line-height: 0.64rem;
-    margin-top: 0.12rem;
-    margin-left: 0.2rem;
-    padding-left: 0.1rem;
-    background-color: #fff;
-    border-radius: 0.1rem;
-    color: #ccc;
-  }
-
-  .header-right {
-    width: 1.24rem;
-    float: right;
-    text-align: center;
-
-    .arrow-icon {
-      font-size: 0.2rem;
-    }
-  }
-}
+.header
+  display: flex
+  line-height: $headerHeight
+  background-color: $bgColor
+  color: #fff
+  .header-left
+    width: 0.62rem
+    float: left
+    .back-icon
+      text-align: center
+      font-size: 0.4rem
+  .header-input
+    flex: 1
+    height: 0.64rem
+    line-height: 0.64rem
+    margin-top: 0.12rem
+    margin-left: 0.2rem
+    padding-left: 0.1rem
+    background-color: #fff
+    border-radius: 0.1rem
+    color: #ccc
+  .header-right
+    width: 1.24rem
+    float: right
+    text-align: center
+    color: #fff
+    .arrow-icon
+      font-size: 0.2rem
 </style>
